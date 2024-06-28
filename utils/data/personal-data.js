5961d1fa7f51435e4341c28ff74e4e413f79dce4
Mkohler4 <Mkohler4@uwo.ca>
2024-06-27 22:54:34 -0400

    Updated resume share file

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,8 @@
 export const personalData = {
   name: "Markus Kohler",
   profile: '/profile.png',
-  designation: "Software Developer",
-  description: "Markus Kohler is a dynamic software engineer and AI enthusiast from Ontario, Canada. After graduating from Western University with a degree in Software Engineering, he founded Pollar, a social media app for interactive polling. Markus is passionate about real-time technology and AI, continually exploring innovative solutions in various industries, including gaming and live events. His expertise and dedication to cutting-edge technology make him a thought leader and visionary in the tech community.",
+  designation: 'Software Developer',
+  description: 'Markus Kohler is a dynamic software engineer and AI enthusiast from Ontario, Canada. After graduating from Western University with a degree in Software Engineering, he founded Pollar, a social media app for interactive polling. Markus is passionate about real-time technology and AI, continually exploring innovative solutions in various industries, including gaming and live events. His expertise and dedication to cutting-edge technology make him a thought leader and visionary in the tech community.',
   email: 'markuskohler40@gmail.com',
   phone: '5192407381',
   address: 'Cambridge, Ontario',
@@ -11,7 +11,7 @@ export const personalData = {
   linkedIn: 'https://www.linkedin.com/in/markus-kohler9/',
   twitter: '',
   stackOverflow: '',
-  leetcode: "",
-  devUsername: "",
-  resume: ""
+  leetcode: '',
+  devUsername: '',
+  resume: 'https://drive.google.com/file/d/11h3x-xUK3corkOveP14Mt2KH1jZbFDiz/view?usp=sharing'
 }
